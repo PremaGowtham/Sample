14ab7c26368a48e646c7f02f2695f1907036144c
PremaGowtham <premalathamegala@gmail.com>
2024-08-17 17:59:17 +0530

    sample commited

---
+++ JavaTraining/src/javapackage/Demo.java
@@ -0,0 +1,11 @@
+package javapackage;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello welcome to Automation");
+
+	}
+
+}
---
+++ JavaTraining/src/javapackage/Sub.java
@@ -0,0 +1,13 @@
+package javapackage;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		
+		int a = 30;
+		int b = 10;
+		int sub= a-b;
+		System.out.println("difference between two number" + sub);
+	}
+
+}
